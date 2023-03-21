d35dc6c41796dc1742c815cfa2b488094bad0d34
AgonMisini23 <agon23misini@gmail.com>
2023-03-21 14:28:38 +0100

    Menu List Screen

--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,31 +1,25 @@
-import { View, Text, Image, FlatList, StyleSheet } from "react-native";
+import { View, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-
+import DishListItem from "../../components/DishListItem";
 import styles from "./styles";
 import restaurants from "../../../assets/data/restaurants.json";
+import RestaurantHeader from "../../components/Header";
 const restaurant = restaurants[0];
 
 const RestaurantDetailsPage = () => {
   return (
     <View style={styles.page}>
-      <Image
-        source={{ uri: restaurant.image }}
-        style={styles.image}
+      <FlatList
+        ListHeaderComponent={() => <RestaurantHeader restaurant={restaurant} />}
+        data={restaurant.dishes}
+        renderItem={({ item }) => <DishListItem dish={item} />}
       />
-
       <Ionicons
         name='arrow-back-circle'
         size={45}
         color='white'
         style={styles.iconContainer}
       />
-      <View style={styles.container}>
-        <Text style={styles.title}>{restaurant.name}</Text>
-        <Text style={styles.subtitle}>
-          ${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-
-          {restaurant.maxDeliveryTime} minutes
-        </Text>
-      </View>
     </View>
   );
 };
--- UberEatsUser/src/screens/RestaurantDetailsScreen/styles.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/styles.js
@@ -4,22 +4,6 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
   },
-  image: {
-    width: "100%",
-    aspectRatio: 5 / 3,
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  subtitle: {
-    fontSize: 15,
-    color: "#525252",
-  },
-  container: {
-    margin: 10,
-  },
   iconContainer: {
     position: "absolute",
     top: 40,
